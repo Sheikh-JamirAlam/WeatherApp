ead2ebb05b1fcb012dea6015c83e939e9c1135ac
Sheikh-JamirAlam <alamjamir15@gmail.com>
2023-02-06 00:44:28 +0530

    Day 4 progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import cityTimezones from 'city-timezones';
+import Loader from "./components/Loader";
 
 function App() {
   const [data, setData] = useState({
@@ -20,6 +21,7 @@ function App() {
       "humidity": 53
     }
   });
+  const [isLoading, setIsLoading] = useState(true);
   const [isSearchDisabled, setIsSearchDisabled] = useState(true);
   const [isClickedSearch, setIsClickedSearch] = useState(false);
   const [handleChange, setHandleChange] = useState("");
@@ -28,6 +30,10 @@ function App() {
   const [date, setDate] = useState(new Date());
   const [time, setTime] = useState("");
   const [icon, setIcon] = useState("clear-day");
+
+  useEffect(() => {
+    getLocation();
+  }, []);
 
   useEffect(() => {
     if (!handleError) {
@@ -43,7 +49,7 @@ function App() {
       setTimeZoneName(searchedCity[0].timezone);
       setTime(new Intl.DateTimeFormat('en-US', options).format(date));
       const timerId = setInterval(refreshDate, 1000);
-      return function cleanup() {
+      return () => {
         clearInterval(timerId);
       };
     }
@@ -105,7 +111,6 @@ function App() {
       case "Haze":
         setIcon("haze");
         break;
-        break;
       case "Fog":
         setIcon("fog");
         break;
@@ -124,6 +129,20 @@ function App() {
     }
   }
 
+  async function getLocation() {
+    const url = `https://ipapi.co/json/`;
+    const response = await fetch(url, {
+      method: "GET"
+    });
+    if (response.ok) {
+      const userData = await response.json();
+      await handleSearch(userData.city);
+      setIsLoading(false);
+    } else {
+      console.log("Failed to fetch data");
+    }
+  }
+
   async function handleSearch(location) {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${import.meta.env.VITE_API_KEY}&units=metric`;
     const response = await fetch(url, {
@@ -139,75 +158,80 @@ function App() {
   }
 
   return (
-    <div
-      className={`h-full w-full flex bg-center`}
-      style={{
-        backgroundImage: `${isClickedSearch ? `url("./assets/${data.weather[0].main.toLowerCase()}.jpg")` : 'url("./assets/clear.jpg")'}`
-      }}
-    >
-      <div className="flex flex-col place-content-center mx-auto font-josefin font-normal text-lg text-gray-200">
-        <div className="place-content-evenly w-128 h-128 mb-12 flex flex-col rounded-xl glass-card">
-          <div className="m-5 flex place-content-between">
-            <h3 className="ml-5">{data.name}, {data.sys.country}</h3>
-            <div className="flex">
-              <span className="material-symbols-outlined text-base text-gray-300">schedule</span>
-              <p className="mr-5 ml-1">{time}</p>
-            </div>
-          </div>
-          <div className="m-5 flex flex-col justify-center">
-            <img src={`https://basmilius.github.io/weather-icons/production/fill/all/${icon}.svg`} alt="weather icon" className="w-52 place-self-center" />
-            <p className="place-self-center">{data.weather[0].main}</p>
-          </div>
-          <div className="m-5 flex place-content-between">
-            <div className="ml-5">
+    <>
+      <Loader isVisible={isLoading} />
+      <div
+        className={`h-full w-full flex bg-center ${isLoading ? 'invisible' : 'visible'}`}
+        style={{
+          backgroundImage: `url("./src/assets/${data.weather[0].main.toLowerCase()}.jpg")`
+        }}
+      >
+        <div className="flex flex-col place-content-center mx-auto font-josefin font-normal text-lg text-gray-200">
+          <div className="place-content-evenly w-128 h-128 mb-12 flex flex-col rounded-xl glass-card">
+            <div className="m-5 flex place-content-between">
+              <h3 className="ml-5">{data.name}, {data.sys.country}</h3>
               <div className="flex">
-                <span className="material-symbols-outlined text-gray-300">air</span>
-                <p className="ml-3">{Math.floor(data.wind.speed)} m/s</p>
-              </div>
-              <div className="flex">
-                <span className="material-symbols-outlined text-gray-300">water_drop</span>
-                <p className="ml-3">{data.main.humidity}%</p>
-              </div>
-              <div className="flex">
-                <span className="material-symbols-outlined text-gray-300">waves</span>
-                <p className="ml-3">{data.main.pressure} hPa</p>
+                <span className="material-symbols-outlined text-base text-gray-300">schedule</span>
+                <p className="mr-5 ml-1">{time}</p>
               </div>
             </div>
-            <div className="mr-5 flex flex-col">
-              <h1 className="text-6xl">{Math.floor(data.main.temp)}°</h1>
-              <p className="text-gray-300">Feels like {Math.floor(data.main.feels_like)}°</p>
+            <div className="m-5 flex flex-col justify-center">
+              <img src={`https://basmilius.github.io/weather-icons/production/fill/all/${icon}.svg`} alt="weather icon" className="w-52 place-self-center" />
+              <p className="place-self-center">{data.weather[0].main}</p>
+            </div>
+            <div className="m-5 flex place-content-between">
+              <div className="ml-5">
+                <div className="flex">
+                  <span className="material-symbols-outlined text-gray-300">air</span>
+                  <p className="ml-3">{Math.floor(data.wind.speed)} m/s</p>
+                </div>
+                <div className="flex">
+                  <span className="material-symbols-outlined text-gray-300">water_drop</span>
+                  <p className="ml-3">{data.main.humidity}%</p>
+                </div>
+                <div className="flex">
+                  <span className="material-symbols-outlined text-gray-300">waves</span>
+                  <p className="ml-3">{data.main.pressure} hPa</p>
+                </div>
+              </div>
+              <div className="mr-5 flex flex-col">
+                <h1 className="text-6xl">{Math.floor(data.main.temp)}°</h1>
+                <p className="text-gray-300">Feels like {Math.floor(data.main.feels_like)}°</p>
+              </div>
             </div>
           </div>
-        </div>
-        <div
-          className={`w-14 h-14 mx-auto flex glass-card place-content-center ${!isSearchDisabled ? "justify-between animate-expand" : "items-center cursor-pointer hover:scale-110"} rounded-full transition-transform ease-in-out delay-50 duration-300`}
-          onClick={event => {
-            setIsSearchDisabled(false);
-          }}
-        >
-          <span className={`material-symbols-outlined text-3xl place-self-center ml-3 cursor-pointer ${!isSearchDisabled ? "visible relative" : "invisible absolute"}`}>home_pin</span>
-          <input
-            type="text"
-            name="searchtext"
-            placeholder="Search by city name."
-            autoComplete="off"
-            value={handleChange}
-            className={`h-8 w-96 pl-2 bg-transparent place-self-center outline-none border-b-2 border-gray-500 focus:border-gray-300 ${handleError && "border-red-600 focus:border-red-600"} ${!isSearchDisabled ? "visible relative" : "invisible absolute"}`}
-            onChange={event => {
-              setHandleChange(event.target.value);
-              setHandleError(false);
-            }}
-          />
-          <span
-            className={`material-symbols-outlined text-3xl place-self-center cursor-pointer ${!isSearchDisabled && "mr-3"} ${isSearchDisabled && "pointer-events-none"}`}
+          <div
+            className={`w-14 h-14 mx-auto flex glass-card place-content-center ${!isSearchDisabled ? "justify-between animate-expand" : "items-center cursor-pointer hover:scale-110"} rounded-full transition-transform ease-in-out delay-50 duration-300`}
             onClick={event => {
-              handleSearch(handleChange);
-              setIsClickedSearch(true);
+              setIsSearchDisabled(false);
             }}
-          >search</span>
-        </div>
-      </div >
-    </div>
+          >
+            <span
+              className={`material-symbols-outlined text-3xl place-self-center ml-3 cursor-pointer ${!isSearchDisabled ? "visible relative" : "invisible absolute"}`}
+            >home_pin</span>
+            <input
+              type="text"
+              name="searchtext"
+              placeholder="Search by city name."
+              autoComplete="off"
+              value={handleChange}
+              className={`h-8 w-96 pl-2 bg-transparent place-self-center outline-none border-b-2 border-gray-500 focus:border-gray-300 ${handleError && "border-red-600 focus:border-red-600"} ${!isSearchDisabled ? "visible relative" : "invisible absolute"}`}
+              onChange={event => {
+                setHandleChange(event.target.value);
+                setHandleError(false);
+              }}
+            />
+            <span
+              className={`material-symbols-outlined text-3xl place-self-center cursor-pointer ${!isSearchDisabled && "mr-3"} ${isSearchDisabled && "pointer-events-none"}`}
+              onClick={event => {
+                handleSearch(handleChange);
+                setIsClickedSearch(true);
+              }}
+            >search</span>
+          </div>
+        </div >
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+const Loader = (props) => {
+    return (
+        <div className={`h-screen w-screen flex justify-center items-center bg-neutral-200 ${(!props.isVisible) && "absolute invisible"}`}>
+            <img src="./src/assets/loading.svg" alt="Loading" />
+        </div>
+    );
+}
+
+export default Loader;
